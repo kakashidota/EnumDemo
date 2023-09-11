c6a65900065a068a95f68d75e863c0f084ef43ef
Robin Kamo <robin.kamo@iths.se>
2023-09-11 11:01:38 +0200

    Added the program

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -1,14 +1,72 @@
 ﻿namespace EnumDemo
 {
+
+    enum Beverage
+    {
+        Coffee,
+        Tea,
+        Water,
+        Soda,
+        Juice
+    }
+
+    
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej från cloud23");
-            Console.WriteLine("Detta är version2");
-            Console.WriteLine("Rad 3");
-            Console.WriteLine("Rad 4");
-            Console.WriteLine("Rad 5");
+
+            Console.WriteLine("Welcome to the awsome vending machine");
+            Console.WriteLine("Which beverage do you prefer? (Coffee, Tea, Water, Soda, Juice)");
+            String userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+                case "coffee":
+                    Respond(Beverage.Coffee);
+                    break;
+                case "tea":
+                    Respond(Beverage.Tea);
+                    break;
+                case "water":
+                    Respond(Beverage.Water);
+                    break;
+                case "soda":
+                    Respond(Beverage.Soda);
+                    break;
+                case "juice":
+                    Respond(Beverage.Juice);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, ths drink does not exist");
+                    break;
+
+            }
+
+        }
+
+        static void Respond(Beverage drink)
+        {
+            switch (drink)
+            {
+                case Beverage.Coffee:
+                    Console.WriteLine("You ordered Kaffe latte");
+                    break;
+                case Beverage.Tea:
+                    Console.WriteLine("You ordered Tea, jukky");
+                    break;
+                case Beverage.Water:
+                    Console.WriteLine("You choose water, why though?");
+                    break;
+                case Beverage.Soda:
+                    Console.WriteLine("Nice choioce, bebsi cola");
+                    break;
+                case Beverage.Juice:
+                    Console.WriteLine("One orange juice comming your way!");
+                    break;
+            }
         }
     }
 }
