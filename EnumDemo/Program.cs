dbf525403be5afeeb8b1f4571346993dcf9751e9
Robin Kamo <robin.kamo@iths.se>
2023-09-11 09:57:59 +0200

    Add project files.

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EnumDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Cloud23!");
+        }
+    }
+}
