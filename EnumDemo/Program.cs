1e0498addc2a12399f5820695bee9b4df56113f0
Robin Kamo <robin.kamo@iths.se>
2023-09-11 10:28:50 +0200

    Added two more rows

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hej från cloud23");
             Console.WriteLine("Detta är version2");
+            Console.WriteLine("Rad 3");
+            Console.WriteLine("Rad 4");
         }
     }
 }
