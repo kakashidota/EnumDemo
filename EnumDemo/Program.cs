c96b3aea80de53b7171199936a5bbe2665cdfa7a
Robin Kamo <robin.kamo@iths.se>
2023-09-11 10:21:35 +0200

    made two console write lines

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Cloud23!");
+            Console.WriteLine("Hej från cloud23");
+            Console.WriteLine("Detta är version2");
         }
     }
 }
