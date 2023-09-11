3760ad2cd2be8bcd5d968bb6119756567e6d822f
Robin Kamo <robin.kamo@iths.se>
2023-09-11 10:30:59 +0200

    Added row 5

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("Detta är version2");
             Console.WriteLine("Rad 3");
             Console.WriteLine("Rad 4");
+            Console.WriteLine("Rad 5");
         }
     }
 }
